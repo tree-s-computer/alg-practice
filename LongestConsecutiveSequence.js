0ba6c4362c1e68482374114d3ef21287befd6135
tree <hhyjs2r@gmail.com>
2024-02-24 17:29:43 +0900

    solve LongestConsecutive

---
+++ LongestConsecutiveSequence.js
@@ -0,0 +1,27 @@
+// Given an unsorted array of integers nums,
+// return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+
+const longestConsecutive = (nums) => {
+  if (!nums.length) return 0;
+  const x = [...new Set(nums)].sort((a, b) => a - b);
+  const answer = [];
+  let cnt = 0;
+
+  x.forEach((v, i) => {
+    if (v + 1 === x[i + 1]) {
+      cnt += 1;
+    } else {
+      answer.push(cnt);
+      cnt = 0;
+    }
+  });
+
+  return Math.max(...answer) + 1;
+};
+
+const nums = [1, 2, 0, 1];
+const x = longestConsecutive(nums);
+console.log(x);
+
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
