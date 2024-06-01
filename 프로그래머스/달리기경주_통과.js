88d2c7aa839ae722ef3a26f84525c259a763a89c
tree <hhyjs2r@gmail.com>
2024-06-01 20:04:54 +0900

    docs: add description

--- 프로그래머스/달리기경주_통과.js
+++ 프로그래머스/달리기경주_통과.js
@@ -7,20 +7,23 @@ function solution(players, callings) {
 
   players.forEach((player, index) => {
     playerPositions[player] = index;
-  });
+  }); //경기 기록
 
   callings.forEach((callingPlayer) => {
     const currentIndex = playerPositions[callingPlayer];
     if (currentIndex > 0) {
+      //첫번째 제외
       const previousPlayer = playersArray[currentIndex - 1];
 
-      playersArray[currentIndex - 1] = callingPlayer;
+      playersArray[currentIndex - 1] = callingPlayer; //배열 바꾸고
       playersArray[currentIndex] = previousPlayer;
 
-      playerPositions[callingPlayer]--;
+      playerPositions[callingPlayer]--; //포지션 위치 기록
       playerPositions[previousPlayer]++;
     }
   });
+
+  // 요 약 : 객체에 등수 기록해두고 -> 선수 두명 찾고-> 배열 바꾸고 -> 등수판 업데이트
 
   return playersArray;
 }
